29f3684beb9862db0e35e50bd4a0b07e573acd42
RK <Rktest>
2017-10-31 13:23:05 +0100

    Change1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine(" Hello I'm Nadia");
+            Console.Read();
         }
     }
 }
